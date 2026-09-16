63f25c36ce44bec5b93394d1bd527bc50526e4d3
MonzurMorshed <monzurmorshedcse@gmail.com>
2026-09-16 21:52:53 +0600

    pull from master

--- kidurabd/settings.py
+++ kidurabd/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'kidurabd_db',
         'USER': 'postgres',
-        'PASSWORD': '[PASSWORD]',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432',
     }
